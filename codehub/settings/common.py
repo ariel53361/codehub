6842c0ca26a5ccfb528ee16072247e142b769466
ariel53361 <ariel53361@gmail.com>
2024-09-02 17:25:01 +0300

    changing the media root

--- codehub/settings/common.py
+++ codehub/settings/common.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
 STATIC_URL = "/static/"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = 'media'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 MIDDLEWARE = [
