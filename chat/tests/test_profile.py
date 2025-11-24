fa32f94ce3ad8ff14c845a86f663faab08f7151c
ariel <ariel53361@gmail.com>
2025-11-24 21:22:32 +0200

    add tests for profile update functionality

---
+++ chat/tests/test_profile.py
@@ -0,0 +1,59 @@
+from io import BytesIO
+import os
+from PIL import Image
+import pytest
+from django.core.files.uploadedfile import SimpleUploadedFile
+from rest_framework import status
+
+def generate_test_image_file(name="new_avatar.jpg"):
+    file_obj = BytesIO()
+    image = Image.new("RGB", (10, 10), "white")
+    image.save(file_obj, format="JPEG")
+    file_obj.seek(0)
+    return SimpleUploadedFile(
+        name=name,
+        content=file_obj.getvalue(),
+        content_type="image/jpeg"
+    )
+
+@pytest.mark.django_db
+class TestUpdateProfile:
+    def test_if_data_is_valid_return_200(self, api_client, authenticate):
+        user = authenticate(is_staff=False)
+        profile = user.profile
+        image_file = generate_test_image_file()
+        data = {
+            "bio": "aaa",
+            "avatar": image_file,
+        }
+
+        response = api_client.patch(
+            '/codehub/profile/me/',
+            data=data,
+            format='multipart',
+        )
+        profile.refresh_from_db()
+        expected_avatar_name = os.path.basename(profile.avatar.name)
+        
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': profile.id,
+            'user_id': user.id,
+            'avatar': expected_avatar_name,
+            'bio': data["bio"]
+        }
+
+    def test_if_data_is_invalid_return_400(self, api_client, authenticate):
+        authenticate(is_staff=False)
+        data = {
+            "avatar": "a"
+        }
+
+        response = api_client.patch(
+            '/codehub/profile/me/',
+            data=data,
+            format='multipart',
+        )
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert isinstance(response.data["avatar"], list)
